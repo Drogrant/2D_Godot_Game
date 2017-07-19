7b2eb27f4981aa00e1bcd5b07d2bbdab271b753e
Héctor <Drogrant@users.noreply.github.com>
2017-07-19 19:20:11 +0200

    Add files via upload

--- Scripts/Basic_Character.gd
+++ Scripts/Basic_Character.gd
@@ -5,8 +5,10 @@ export var health = 100
 
 #For now I will place the input here to have quick results but a input manager would be better
 onready var input = preload("Character_Input.gd").new()
-var direction_X = 0.0
-var direction_Y = 0.0
+onready var animator = find_node("Animator")
+onready var sprite = find_node("Sprite")
+
+var direction = Vector2(0.0,0.0)
 
 func _ready():
 	input.owner = self
@@ -16,5 +18,17 @@ func _ready():
 func _input(event):
 	input.handle_input(event)
 
+func change_direction(mod_direction):
+	direction += mod_direction
+	if(direction.length() == 0):
+		animator.stop(false)
+		return
+	else:
+		var identify = -3*direction.dot(Vector2(0,1)) + 2*direction.dot(Vector2(1,0))
+		if(identify == 3): animator.play("Walk_up"); return
+		if(identify == -3): animator.play("Walk_Down"); return
+		if(identify == 2): sprite.set_flip_h(true); animator.play("Walk_left"); return
+		if(identify == -2): sprite.set_flip_h(false); animator.play("Walk_left");
+
 func _fixed_process(delta):
-	move(Vector2(direction_X,direction_Y).normalized()*speed*delta)
+	move(direction.normalized()*speed*delta)
--- Scripts/Character_Input.gd
+++ Scripts/Character_Input.gd
@@ -1,38 +1,31 @@
 var owner
 
-var r_or_l = true
-var u_or_d = true
-
 #Quick movement input, I am sure that there is a better way, but it works.
 
 func handle_input(event):
 	if(event.type == InputEvent.KEY):
 		if(event.is_pressed()):
 			if(event.is_action_pressed("ui_up")):
-				owner.direction_Y = -1.0
-				u_or_d = true
+				owner.change_direction(Vector2(0,-1))
 				return
 			if(event.is_action_pressed("ui_right")):
-				owner.direction_X = 1.0
-				r_or_l = true
+				owner.change_direction(Vector2(1,0))
 				return
 			if(event.is_action_pressed("ui_down")):
-				owner.direction_Y = 1.0
-				u_or_d = false
+				owner.change_direction(Vector2(0,1))
 				return
 			if(event.is_action_pressed("ui_left")):
-				owner.direction_X = -1.0
-				r_or_l = false
+				owner.change_direction(Vector2(-1,0))
 				return
 		else:
-			if(event.is_action_released("ui_up") and u_or_d): 
-				owner.direction_Y = 0.0
+			if(event.is_action_released("ui_up")): 
+				owner.change_direction(Vector2(0,1))
 				return
-			if(event.is_action_released("ui_down") and !u_or_d):
-				owner.direction_Y = 0.0
+			if(event.is_action_released("ui_down")):
+				owner.change_direction(Vector2(0,-1))
 				return
-			if(event.is_action_released("ui_left") and !r_or_l):
-				owner.direction_X = 0.0
+			if(event.is_action_released("ui_left")):
+				owner.change_direction(Vector2(1,0))
 				return
-			if(event.is_action_released("ui_right") and r_or_l):
-				owner.direction_X = 0.0
+			if(event.is_action_released("ui_right")):
+				owner.change_direction(Vector2(-1,0))
